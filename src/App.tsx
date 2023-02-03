4396dc27c48e59d132ac7ed1a4045098a0255608
Allan Alves <allan.junior-ext@viavarejo.com.br>
2023-02-03 18:19:08 -0300

    chore: adição do styled components, configuração do eslint e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-function App() {
+import styled, { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
+export const Button = styled.button`
+  padding: 1rem;
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.secondary};
+`
+
+function App() {
   return (
-    <h1>...</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>...</h1>
+      <Button>Allan</Button>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
